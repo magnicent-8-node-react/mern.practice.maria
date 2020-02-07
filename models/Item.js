af0eb2b1c33996a186a775c7695d6fe3e6da0017
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2020-02-07 14:06:23 -0800

    edit: make changes to all files

--- models/Item.js
+++ models/Item.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-const Model = mongoose.Schema
+const Model = mongoose.Schema;
 
-// create Schema
-const ItemModel = new Model({
-    name: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-});
+// Create Schema
+const ItemSchema = new Model({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});  
 
-module.exports = Item = mongoose.model('item', ItemModel);
+module.exports = Item = mongoose.model('item', ItemSchema);
--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 const app = express();
 
 // Bodyparser Middleware
 app.use(bodyParser.json())
 
 // DB Config
-const db = require('./config/keys').mongoURI
+const db_url = process.env.MONGODB_URL || require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-  .connect(db, { 
+  .connect(db_url, { 
     useNewUrlParser: true,
     useCreateIndex: true
   }) // Adding new mongo url parser
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use('/api/items', require('./routes/api/items'));
+// Use Routes
+app.use('/api/items', cors(), require('./routes/api/items'));
 
+// Listen on Port
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
